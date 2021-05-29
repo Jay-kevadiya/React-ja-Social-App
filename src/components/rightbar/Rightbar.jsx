0bc6d6e781db11b61059bc4b67796f1b8deca06c
Jay <jaykevadiya7@gmail.com>
2021-05-29 10:48:29 +0530

    init

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -30,8 +30,53 @@ export default function Rightbar({profile}) {
             <>
                 <h4 className="rightbarTitle">User Information</h4>
                 <div className="rightbarInfo">
-                    
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">City:</span>
+                        <span className="rightbarInfoValue">New York</span>
+                    </div>
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">From:</span>
+                        <span className="rightbarInfoValue">Madrid</span>
+                    </div>
+                    <div className="rightbarInfoItem">
+                        <span className="rightbarInfoKey">Relationship:</span>
+                        <span className="rightbarInfoValue">Signle</span>
+                    </div>
                 </div>
+
+                <h4 className="rightbarTitle">User friends</h4>
+                <div className="rightbarFollowings">
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">Jay Kevadiya</span>
+                    </div>
+
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/2.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">Jay Kevadiya</span>
+                    </div>
+
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/3.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">Jay Kevadiya</span>
+                    </div>
+
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/4.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">Jay Kevadiya</span>
+                    </div>
+
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/5.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">Jay Kevadiya</span>
+                    </div>
+
+                    <div className="rightbarFollowing">
+                        <img src="assets/person/6.jpeg" alt="" className="rightbarFollowingImg" />
+                        <span className="rightbarFollowingName">Jay Kevadiya</span>
+                    </div>
+                </div>
+
             </>
             )
     }
